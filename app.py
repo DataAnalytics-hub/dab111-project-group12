2f0cae90fa15f151f59adf50dbc0ad5838af6a0e
DataAnalytics-hub <groupsaintclair@gmail.com>
2024-12-13 16:09:06 -0500

    final version (probably will change)

---
+++ app.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timezone
+from flask import Flask, Response, g, render_template
+from matplotlib.backends.backend_agg import FigureCanvasAgg as Canvas
+from matplotlib.figure import Figure
+import matplotlib as mpl, matplotlib.pyplot as plt
+import io, sqlite3, pandas as pd, seaborn as sns
+plt.style.use('ggplot')
+mpl.use('Agg')
+
+app = Flask(__name__)
+app.config.setdefault('TICKERS', {
+    'AAPL': 'Apple Inc. Stock Value in United States Dollar',
+    'NVDA': 'Nvidia Corp. Stock Value in United States Dollar',
+    'MSFT': 'Microsoft Corp. Stock Value in United States Dollar',
+    'AMZN': 'Amazon.com Inc. Stock Value in United States Dollar',
+})
+
+def db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('./database.db').cursor()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.connection.close()
+
+
+@app.get('/')
+@app.get('/about')
+def about():
+    g.nav_active = 'about'
+    return render_template('about.html')
+
+@app.get('/dataset')
+def dataset():
+    g.nav_active = 'dataset'
+    datasets = dict()
+    for ticker, desc in app.config['TICKERS'].items():
+        data = db().execute(f'SELECT * FROM {ticker.lower()} ORDER BY timestamp_ DESC LIMIT 5').fetchall()
+        data = list(map(lambda x: (datetime.fromtimestamp(x[0], timezone.utc).strftime('%Y-%m-%d %H:%M'), *x[1:]), data))
+        count = db().execute(f'SELECT COUNT(*) FROM {ticker.lower()}').fetchone()[0]
+        datasets[ticker] = {'data': data, 'count': count, 'description': desc}
+    return render_template('dataset.html', datasets = datasets)
+
+@app.get('/plots')
+def plots():
+    g.nav_active = 'plots'
+    return render_template('visuals.html', names = app.config.get('TICKERS').keys())
+
+def get_dataset(ticker):
+    data = db().execute(f'SELECT timestamp_, close_ FROM {ticker.lower()} ORDER BY timestamp_').fetchall()
+    df = pd.DataFrame(data, columns=['timestamp', 'closing_price'])
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+    return (app.config.get('TICKERS')[ticker], df)
+
+@app.get('/plot/<string:idx>')
+def plot(idx):
+    fig = Figure((10, 6), dpi=120)
+    ax = fig.add_subplot(1,1,1)
+    desc, df = get_dataset(idx)
+    sns.lineplot(df, x='timestamp', y='closing_price', linewidth=1, ax=ax)
+    ax.set_title(desc)
+    ax.set_xlabel('Timeline')
+    ax.set_ylabel('Price ($)')
+    buff = io.BytesIO()
+    Canvas(fig).print_png(buff)
+    return Response(buff.getvalue(), mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True, port=3300)
---
+++ datacollection.py
@@ -0,0 +1,51 @@
+import polygon, sqlite3, time
+from credentials import polygon_api_key
+
+print('''
+************************************************
+*                 WARNING!!!                   *
+*    This script will take more than 4min to   *
+*       complete. Please run with caution.     *
+************************************************
+''')
+# Thought to make data collection and api key management part of the website too.
+# But no one likes to stare at a loading screen for 5min just to get the data.
+
+
+###################################################################################
+# Change the below parameters as required to update the dataset
+tickers = ['AAPL', 'NVDA', 'MSFT', 'AMZN']
+multiplier = 5
+timespan = 'minute'
+from_ = '2024-01-01'
+to_ = '2024-12-10'
+
+
+schema = '''
+    CREATE TABLE IF NOT EXISTS msft(timestamp_ INTEGER, open_ NUMERIC, close_ NUMERIC, high_ NUMERIC, low_ NUMERIC);
+    CREATE TABLE IF NOT EXISTS amzn(timestamp_ INTEGER, open_ NUMERIC, close_ NUMERIC, high_ NUMERIC, low_ NUMERIC);
+    CREATE TABLE IF NOT EXISTS aapl(timestamp_ INTEGER, open_ NUMERIC, close_ NUMERIC, high_ NUMERIC, low_ NUMERIC);
+    CREATE TABLE IF NOT EXISTS nvda(timestamp_ INTEGER, open_ NUMERIC, close_ NUMERIC, high_ NUMERIC, low_ NUMERIC);
+'''
+
+client = polygon.RESTClient(polygon_api_key)
+
+with sqlite3.connect('database.db') as conn:
+    cur = conn.cursor()
+    cur.executescript(schema)
+    conn.commit()
+    for ticker in tickers:
+        print(f'Collecting {ticker}')
+        data_count = 0
+        try: 
+            for row in client.list_aggs(ticker, 5, 'minute', '2024-01-01', '2024-12-10', True, 'desc', 50000):
+                entry = (row.timestamp//1000,row.open,row.close,row.high,row.low)
+                cur.execute(f'INSERT INTO {ticker.lower()} VALUES(?, ?, ?, ?, ?)', entry)
+                data_count += 1
+            print(f'Fetched {data_count} Entries for {ticker}')
+        except Exception as e:
+            print(e)
+        finally:
+            conn.commit()
+        if ticker != tickers[-1]:
+            time.sleep(60) # Yes, this is necessary due to that damn API calls limit.
